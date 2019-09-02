2165a1704af902f8c2e44cdecafd092d04c85aab
stbn <stbnrivas@gmail.com>
2019-09-02 11:27:36 +0200

    update deploy to use with systemd

--- deploy/run-bot.sh
+++ deploy/run-bot.sh
@@ -7,9 +7,6 @@ LOG_FILE="mastodon_bot.log"
 /usr/bin/docker run \
   --name santander-bot \
   --rm \
-  --env-file=./bot.env \
-  --volume ${PWD}/${LOG_FILE}:/bot/${LOG_FILE} \
+  --env-file=/bot/bot.env \
+  --volume /bot/${LOG_FILE}:/bot/${LOG_FILE} \
   ${DOCKER_IMG}:${DOCKER_TAG}
-
-
-  # --mount type=bind,source=${PWD}/mastodon_bot.log,target=/boot/mastodon_bot.log \
